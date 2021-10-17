580db594d49060d3c159d29894c12d262d884ecf
survaseshubham595 <92672441+survaseshubham595@users.noreply.github.com>
2021-10-17 17:19:00 +0530

    Update StringCalculator.java

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -5,9 +5,13 @@ class StringCalculator {
     public int add(String input) {
          if (string.IsNullOrEmpty(input))
          {
-             //if numbers empty then return 
+             //if numbers empty then return 0
         return 0;
     }
+         var delimeters = new List<char>()
+                                    {
+                                        '\n',',' //numbers separated by commas
+                                    };
       
     }
 }
