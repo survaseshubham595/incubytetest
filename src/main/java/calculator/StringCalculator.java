d91c3c3a63c50ffe181ccfa21a9db449c9f7faa3
survaseshubham595 <92672441+survaseshubham595@users.noreply.github.com>
2021-10-17 17:13:47 +0530

    Update StringCalculator.java

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -3,7 +3,11 @@ package calculator;
 class StringCalculator {
 
     public int add(String input) {
+         if (string.IsNullOrEmpty(input))
+         {
+             //if numbers empty then return 
         return 0;
     }
-
+      
+    }
 }
